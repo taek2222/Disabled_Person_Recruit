8dce0a8498f76cc29dd9e76d0918713b40caf306
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-02-19 02:36:29 +0900

    [Yoo] 헤더 테스트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-import Header from './component/Header/header';
+import Header from './component/Header/header.js';
 
 const App = () => {
   return (
-    <>
+    <div className="w-full container mx-auto bg-white">
       <Header />
-    </>
+    </div>
   );
 }
 
--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const header = () => {
     return (
-        <div></div>
+        <header className="sticky top-0 w-full bg-white z-10">
+            <div className="flex justify-center items-center space-x-6 pt-3 mb-3"></div>
+
+            <div className="w-full h-px bg-gray-400"></div>
+        </header>
     )
 }
 
